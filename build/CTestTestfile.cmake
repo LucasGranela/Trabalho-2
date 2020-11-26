519b7427695880bee8d3be248d6549af75f90753
LucasGranela <lucasgranela2000@usp.br>
2020-11-26 18:17:05 -0300

    sem build

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/logranela/ED3/Trabalho-2
-# Build directory: /home/logranela/ED3/Trabalho-2/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
