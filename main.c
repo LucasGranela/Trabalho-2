fed1ce828f7be2c97fbef15aa6caa6bd4c8121f6
PedroRibasSerras <pedrofhp@usp.br>
2020-11-28 23:42:58 -0300

    comentarios na main

--- main.c
+++ main.c
@@ -49,25 +49,29 @@ int main () {
         binarioNaTela2(nomeArquivo2);
     }
     else if(caso == 7) {
+        //Cria ponteiro do arquivo ordenado e a lista ordenada
         FILE* arquivoSegueOrdenado;
         Lista* li = cria_lista();
 
-        int quantPessoas = 0;
+        //cria variáveis para analisar os cabeçalhos
         char estavel;
         int numeroDeRegistros;
 
+        // Pega o nome dos arquivos
         scanf("%[^ ]%*c%s", nomeArquivo1, nomeArquivo2);
 
+        //Verifica abertura e o cabecalho
         if(!abreArquivo(&arquivoSegue, nomeArquivo1, "rb+\0", 3) || !abreArquivo(&arquivoSegueOrdenado, nomeArquivo2, "wb\0", 3))
             return 0;
 
 
-
+        //le o cabecalho
         fseek(arquivoSegue, 0, SEEK_SET);
         fread(&estavel, sizeof(char), 1, arquivoSegue);
         fread(&numeroDeRegistros, sizeof(int), 1, arquivoSegue);
         fseek(arquivoSegue, 32, SEEK_SET);
 
+        //cria variaveis para armazenar os valores do arquivo
         char removido;
         int idPessoaQueSegue;
         int idPessoaQueESeguida;
@@ -75,8 +79,10 @@ int main () {
         char dataInicioQueSegue[11];
         char dataFimQueSegue[11];
 
+        //repetição para pegar todos os registros do arquivos
         for(int i = 0; i<numeroDeRegistros;i++){
 
+            //Leitura dos registros
             fread(&removido, sizeof(char), 1, arquivoSegue);
             fread(&idPessoaQueSegue, sizeof(int), 1, arquivoSegue);
             fread(&idPessoaQueESeguida, sizeof(int), 1, arquivoSegue);
@@ -87,108 +93,123 @@ int main () {
             fread(dataFimQueSegue, sizeof(char), 10, arquivoSegue);
             dataFimQueSegue[10] = '\0';
 
+            //inserindo de forma organizada no arquivo
             insere_lista_ordenada(li,removido,idPessoaQueSegue,idPessoaQueESeguida,grauAmizade,dataInicioQueSegue,dataFimQueSegue);
 
         }
 
+        //Como vamos conmeçar a escrever no arquivo ordenado, marcamos o cabecalho como instavel 0
         escreveCabArqSegue(arquivoSegueOrdenado, 0, '0');
 
+        //Posicionando o cursor depois do cabecalho
         fseek(arquivoSegueOrdenado, 32, SEEK_SET);
+
+        //adicionando os valores que estão na lisata, direto no arquivo binsorted
         add_bin(li ,arquivoSegueOrdenado, numeroDeRegistros);
 
+        //marcando o cabecalho com estavel, ja que acabamos de escrever os registros
         escreveCabArqSegue(arquivoSegueOrdenado, numeroDeRegistros, '1');
 
+        //fechandio os arquivos abertos
         fclose(arquivoSegueOrdenado);
         fclose(arquivoSegue);
 
+        //imprime o binario na tela
         binarioNaTela2(nomeArquivo2);
     }
     else if (caso == 8) {
 
 
-    //variaveis dos arquivos e o campo a ser utilizado
-    char nomeArqPessoa[50];
-    char nomeArqIndex[50];
-    char campo[15]; //idPessoa,nomePessoa,idadePessoa ou twitterPessoa
-    int idPessoa;
+        //variaveis dos arquivos e o campo a ser utilizado
+        char nomeArqPessoa[50];
+        char nomeArqIndex[50];
+        char campo[15]; //idPessoa,nomePessoa,idadePessoa ou twitterPessoa
+        int idPessoa;
 
 
-    //leitura das variaveis
-    scanf("%[^ ]%*c", nomeArqPessoa);
-    scanf("%[^ ]%*c", nomeArqIndex);
-    scanf("%[^ ]%*c", campo);
-    scanf("%d", &idPessoa);
-    scanf("%s", nomeArquivo1);
+        //leitura das variaveis
+        scanf("%[^ ]%*c", nomeArqPessoa);
+        scanf("%[^ ]%*c", nomeArqIndex);
+        scanf("%[^ ]%*c", campo);
+        scanf("%d", &idPessoa);
+        scanf("%s", nomeArquivo1);
 
-    
 
-    //vai abrir os dois arquivos necessarios e verificar se eles estao de acordo com o necessario
-    FILE* arquivoPessoa = fopen(nomeArqPessoa, "rb");
-    FILE* arquivoIndexaPessoa = fopen(nomeArqIndex, "rb");
-    arquivoSegue = fopen(nomeArquivo1, "rb");
 
-    //verifica se existe um arquivo e se ele abre corretamente
-    if(arquivoPessoa == NULL || arquivoIndexaPessoa == NULL|| arquivoSegue == NULL){
-        printf("Falha no processamento do arquivo.\n");
-        return 0;
-    }
+        //vai abrir os dois arquivos necessarios e verificar se eles estao de acordo com o necessario
+        FILE* arquivoPessoa = fopen(nomeArqPessoa, "rb");
+        FILE* arquivoIndexaPessoa = fopen(nomeArqIndex, "rb");
+        arquivoSegue = fopen(nomeArquivo1, "rb");
 
-    if(!verificaConsistenciaComAPalavraProcessamento(arquivoIndexaPessoa) || !verificaConsistenciaComAPalavraProcessamento(arquivoPessoa)|| !verificaConsistenciaComAPalavraProcessamento(arquivoSegue))
-        return 0;
-
-    //de novo, queria usar o switch case, mas parece que o compilador nao compila direito entao fiz com if else
-    if(strcmp(campo, "idPessoa") == 0) {//verifica se o campo escolhido e' o do idPessoa
-        int RRN = retornaRRNcomId(arquivoIndexaPessoa,idPessoa);
-
-        if(RRN != -1){
-            fseek(arquivoPessoa, ((RRN+1)*64), SEEK_SET);
-            imprimeRegistroNaTela(arquivoPessoa); //imprime o dado na tela
-        }
-        else{
-            printf("Registro inexistente.\n");
-            fclose(arquivoIndexaPessoa);
-            fclose(arquivoPessoa);
+        //verifica se existe um arquivo e se ele abre corretamente
+        if(arquivoPessoa == NULL || arquivoIndexaPessoa == NULL|| arquivoSegue == NULL){
+            printf("Falha no processamento do arquivo.\n");
             return 0;
         }
-        fclose(arquivoIndexaPessoa);
-        fclose(arquivoPessoa);
 
-        int quantPessoas = 0;
-        int numeroDeRegistros;
+        if(!verificaConsistenciaComAPalavraProcessamento(arquivoIndexaPessoa) || !verificaConsistenciaComAPalavraProcessamento(arquivoPessoa)|| !verificaConsistenciaComAPalavraProcessamento(arquivoSegue))
+            return 0;
 
-        fseek(arquivoSegue, sizeof(char), SEEK_SET);
-        fread(&numeroDeRegistros, sizeof(int), 1, arquivoSegue);
-        fseek(arquivoSegue, 32, SEEK_SET);
+        //de novo, queria usar o switch case, mas parece que o compilador nao compila direito entao fiz com if else
+        if(strcmp(campo, "idPessoa") == 0) {//verifica se o campo escolhido e' o do idPessoa
+            int RRN = retornaRRNcomId(arquivoIndexaPessoa,idPessoa);
 
-        char removido;
-        int idPessoaQueSegue;
-        int idPessoaQueESeguida;
-        char grauAmizade[4];
-        char dataInicioQueSegue[11];
-        char dataFimQueSegue[11];
-
-        for(int i = 0; i<numeroDeRegistros;i++){
-
-            fread(&removido, sizeof(char), 1, arquivoSegue);
-            fread(&idPessoaQueSegue, sizeof(int), 1, arquivoSegue);
-            fread(&idPessoaQueESeguida, sizeof(int), 1, arquivoSegue);
-            fread(grauAmizade, sizeof(char), 3, arquivoSegue);
-            grauAmizade[3] = '\0';
-            fread(dataInicioQueSegue, sizeof(char), 10, arquivoSegue);
-            dataInicioQueSegue[10] = '\0';
-            fread(dataFimQueSegue, sizeof(char), 10, arquivoSegue);
-            dataFimQueSegue[10] = '\0';
-
-            if(removido =='1' && idPessoaQueSegue == idPessoa){
-                printf("Segue a pessoa de código: %d \n",idPessoaQueESeguida);
-                printf("Justificativa para seguir: %s\n",interpretadorDeGrau(grauAmizade));
-                printf("Começou a seguir em: %s\n",dataInicioQueSegue);
-                printf("Parou de seguir em: %s\n",dataFimQueSegue);
-                printf("\n");
+            if(RRN != -1){
+                fseek(arquivoPessoa, ((RRN+1)*64), SEEK_SET);
+                imprimeRegistroNaTela(arquivoPessoa); //imprime o dado na tela
             }
+            else{
+                //Se não encontrar nenhum registros, finaliza-se o programa
+                printf("Registro inexistente.\n");
+                fclose(arquivoIndexaPessoa);
+                fclose(arquivoPessoa);
+                return 0;
+            }
+            //fecha os arquivoscom os dados das pessoas
+            fclose(arquivoIndexaPessoa);
+            fclose(arquivoPessoa);
 
+            //variavel para armazenar o numero de registros
+            int numeroDeRegistros;
+
+            //lendo o cabecalho
+            fseek(arquivoSegue, sizeof(char), SEEK_SET);
+            fread(&numeroDeRegistros, sizeof(int), 1, arquivoSegue);
+            fseek(arquivoSegue, 32, SEEK_SET);
+
+            //cria variaveis para armazenar os valores do arquivo
+            char removido;
+            int idPessoaQueSegue;
+            int idPessoaQueESeguida;
+            char grauAmizade[4];
+            char dataInicioQueSegue[11];
+            char dataFimQueSegue[11];
+
+            //repetição para pegar todos os registros do arquivos
+            for(int i = 0; i<numeroDeRegistros;i++){
+
+                //Leitura dos registros
+                fread(&removido, sizeof(char), 1, arquivoSegue);
+                fread(&idPessoaQueSegue, sizeof(int), 1, arquivoSegue);
+                fread(&idPessoaQueESeguida, sizeof(int), 1, arquivoSegue);
+                fread(grauAmizade, sizeof(char), 3, arquivoSegue);
+                grauAmizade[3] = '\0';
+                fread(dataInicioQueSegue, sizeof(char), 10, arquivoSegue);
+                dataInicioQueSegue[10] = '\0';
+                fread(dataFimQueSegue, sizeof(char), 10, arquivoSegue);
+                dataFimQueSegue[10] = '\0';
+
+                // imprimindo apenas os que nao foram removidos e os que a pessoa com o ide requisitado segue
+                if(removido =='1' && idPessoaQueSegue == idPessoa){
+                    printf("Segue a pessoa de código: %d \n",idPessoaQueESeguida);
+                    printf("Justificativa para seguir: %s\n",interpretadorDeGrau(grauAmizade));
+                    printf("Começou a seguir em: %s\n",dataInicioQueSegue);
+                    printf("Parou de seguir em: %s\n",dataFimQueSegue);
+                    printf("\n");
+                }
+
+            }
         }
-    }
     }
     return 0;
 }
