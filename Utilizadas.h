5f262795a273e799d4afeb974f9b9eed37bb72c6
LucasGranela <lucasgranela2000@usp.br>
2020-11-28 22:44:25 -0300

    finalizado

--- Utilizadas.h
+++ Utilizadas.h
@@ -19,5 +19,6 @@
     void insereCSVparaSegue(FILE* arqCSV, FILE* arqSegue, int* quantPessoas);
     void inserirArqSegue(int idPessoaQueSegue, int idPessoaQueESeguida, char graAmizade[3], char dataInicio[10], char dataFim[10],  FILE* arquivoSegue);
     char* interpretadorDeGrau(char grauAmizade[4]);
+    int retornaRRNcomId(FILE* arquivo,int valor);
 
 #endif
--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "arqIndicePessoa.h"
 #include "fornecido.h"
